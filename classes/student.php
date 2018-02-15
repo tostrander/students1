d7f5064aaa82ba440f60f31daf806e3218662b02
Tina Ostrander <tostrander@greenriver.edu>
2018-02-15 14:55:14 -0500

    setters and getters added to Student class

--- classes/student.php
+++ classes/student.php
@@ -19,6 +19,102 @@ class Student
         $this->_advisor = $advisor;
     }
 
+    /**
+     * @return mixed
+     */
+    public function getSid()
+    {
+        return $this->_sid;
+    }
+
+    /**
+     * @param mixed $sid
+     */
+    public function setSid($sid)
+    {
+        $this->_sid = $sid;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getLast()
+    {
+        return $this->_last;
+    }
+
+    /**
+     * @param mixed $last
+     */
+    public function setLast($last)
+    {
+        $this->_last = $last;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getFirst()
+    {
+        return $this->_first;
+    }
+
+    /**
+     * @param mixed $first
+     */
+    public function setFirst($first)
+    {
+        $this->_first = $first;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getBirthdate()
+    {
+        return $this->_birthdate;
+    }
+
+    /**
+     * @param mixed $birthdate
+     */
+    public function setBirthdate($birthdate)
+    {
+        $this->_birthdate = $birthdate;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getGpa()
+    {
+        return $this->_gpa;
+    }
+
+    /**
+     * @param mixed $gpa
+     */
+    public function setGpa($gpa)
+    {
+        $this->_gpa = $gpa;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getAdvisor()
+    {
+        return $this->_advisor;
+    }
+
+    /**
+     * @param mixed $advisor
+     */
+    public function setAdvisor($advisor)
+    {
+        $this->_advisor = $advisor;
+    }
+
     function toString()
     {
         return $this->_first . " " . $this->_last;
